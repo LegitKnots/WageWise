48f439c7bcded4d14a04c1dee1223cecff5685df
Alexander Phillipson <127406648+LegitKnots@users.noreply.github.com>
2025-10-08 06:02:56 -0400

    Config Setup
    added reactotron + typescript config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.ts', '.tsx', '.json'],
+        root: ['.'],
+        alias: {
+          assets: './src/assets/',
+          components: './src/components/',
+          config: './src/config/',
+          context: './src/context/',
+          hooks: './src/hooks/',
+          interfaces: './src/interfaces/',
+          navigation: './src/navigation/',
+          screens: './src/screens/',
+          scripts: './src/scripts/',
+          services: './src/services/',
+          styles: './src/styles/',
+          types: './src/types/',
+        },
+      },
+    ],
+  ],
 };
